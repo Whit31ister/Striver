3c16ac3d3e98d685cb3b60d150d0a9425c9fb342
Whitelister <spikenotfound731@gmail.com>
2026-01-20 17:46:17 +0530

    Grading problem

--- Learn the basics/Grading_System/GradingSys.cpp
+++ Learn the basics/Grading_System/GradingSys.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
+    int marks;
+    cin >> marks;
+    if(marks < 25){
+        cout << "F";
+    } else if (marks > 25 && marks < 45)
+    {
+        cout << "E";
+    } else if (marks > 45 && marks < 49)
+    {
+        cout << "D";
+    }else if (marks > 49 && marks < 59)
+    {
+        cout << "C";
+    }
+    // it continues like this
+    cout << "Heloo world";
 
     return 0;
 }
