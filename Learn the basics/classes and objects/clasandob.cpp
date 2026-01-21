835617d6d41ea5ee33c68096ea1c3dd27aa25673
Whitelister <spikenotfound731@gmail.com>
2026-01-21 11:29:24 +0530

    oop done

---
+++ Learn the basics/classes and objects/clasandob.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// class creation
+class numbers{
+public:
+    int a;
+    int b;
+    int c;
+//class function
+    void outting(){
+        cout << "A:" << a << endl;
+        cout << "B:" << b << endl;
+        cout << "C:" << c << endl;
+    }
+};
+//main function
+int main(){    
+    numbers num; // object declaire 
+
+     int A;
+     int B;
+     int C;
+//input for objects
+     cout << "Enter A: ";
+     cin >> A;
+     cout << "Enter B: ";
+     cin >> B;
+     cout << "Enter C: \n";
+     cin >> C;
+//asign or something
+     num.a = A;
+     num.b = B;
+     num.c = C;
+//class wale function ka call
+     num.outting();
+
+    return 0;
+}
