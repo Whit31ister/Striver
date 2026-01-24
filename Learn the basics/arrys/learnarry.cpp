76f8f288d2fac4a9fc09e522e4c7c0f74afcc05a
Whitelister <spikenotfound731@gmail.com>
2026-01-24 22:42:00 +0530

    arrys

---
+++ Learn the basics/arrys/learnarry.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arry[5];
+    for (size_t i = 0; i < 4; i++)
+    {
+        cin >> arry[i];
+    }
+    cout << arry[2];
+    return 0;
+}
