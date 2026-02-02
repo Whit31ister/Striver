9475a738e1321349de2453956668b8163c77acfc
Whitelister <spikenotfound731@gmail.com>
2026-02-02 11:24:51 +0530

    out test

--- Learn the basics/pattern.cpp
+++ Learn the basics/pattern.cpp
@@ -1,43 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int star_pattern(int count){
-    int j = 0;
-    int i = 0;
-    for (i = 0; i < count; i++)
-    {
-        for (j = 0; j< i; j++)
-        {
-            cout << " * ";
-        }
-        cout << endl;
-    }
-}
-
-int pyramid_pattern(int count){
-    for (int i = 1; i <= count; i++)
-    {
-        for (int space = 1; space <= count-i; space++)
-        {
-            cout << " ";
-        }
-        for (int star = 1; star <= i; star++)
-        {
-            cout << "* ";
-        }
-        cout << endl;   
-    }
-}
-
 int main(){
-    int j = 0;
-    int i = 0;
-    int count;
-    cin >> count;
-    star_pattern(count);
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    pyramid_pattern(count);
+
+    cout << "out test" << endl;
+
     return 0;
 }
