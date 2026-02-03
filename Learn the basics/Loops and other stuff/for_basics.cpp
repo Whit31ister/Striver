3a89cd14c1fd6763e787cefd6dc0f5c8799438c3
Whitelister <spikenotfound731@gmail.com>
2026-02-03 22:44:52 +0530

    done with the basics

---
+++ Learn the basics/Loops and other stuff/for_basics.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 100; i++)
+    {
+        cout << "hEllo 100 times \n";
+    }
+    
+    return 0;
+}
