9e36b17324926773a5192aa7c6bdec97ab7ca511
Whit31ister <Spikenotfound731@gmail.com>
2026-02-06 09:02:02 +0530

    Vector clean up
    starting 6th

--- Cpp-STL/vectors/vectors.cpp
+++ Cpp-STL/vectors/vectors.cpp
@@ -1,14 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, int>> get_pairs(){
-    vector<pair<int, int>> v3;
-    v3.emplace_back({1,2});
-    v3.emplace_back({4,3});
-    return v3;
-} //dawdawd
-
-
 int main(int argc, char const *argv[])
 {
     vector<int> v;
@@ -19,7 +11,6 @@ int main(int argc, char const *argv[])
     vector<pair<int, int>> v1;
     v1.push_back({1,2});
     v1.emplace_back(2,6);
-    return v;
     
     return 0;
 }
